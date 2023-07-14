9791ad59d6c46334162d4fa86da7566acacc9bfc
Harika <harikamanapurapu@gmail.com>
2023-07-14 15:18:13 +0530

    code has been update

---
+++ src/Components/Groups.js
@@ -0,0 +1,132 @@
+//Groups Component which consists the side bar for the groups created by the user
+
+import React from "react";
+import { useState , useEffect } from "react";
+import './Groups.css'
+import Notes from "./Notes";
+import bgimg from './images/notes-img.png'
+import lock from './images/lock.png'
+
+
+function Groups({groups,setGroups,setIsPopUpVisible}){
+
+    //active group Id handles the group which is active
+    const [activeGroupId, setActiveGroupId] = useState(null);
+    //Isvisible and showbutton handles the mobile view to set display properties in react
+    const [isVisible, setIsVisible] = useState(false);
+    const [showButton, setShowButton] = useState(true);
+
+    
+    // Save groups to local storage when groups state changes
+    useEffect(() => {
+        localStorage.setItem("groups", JSON.stringify(groups));
+    }, [groups]);
+
+    //Handles the click when the group name is clicked, sets to active and opens its respective notes and handles mobile view too.
+    const handleGroupClick = (groupId) => {
+      setActiveGroupId(groupId);
+      setIsVisible(true);
+      if(window.innerWidth>768){
+        setShowButton(true)
+      }
+      else{
+        setShowButton(false)
+      }
+    };
+
+    //Gets time stamp when notes is added to the notes container
+    const getTimeStamp = () => {
+      const date = new Date();
+      const day = date.getDate();
+      const month = date.toLocaleString('en-US', { month: 'long' });
+      const year = date.getFullYear();
+      const time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+      const formattedTime = time.replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+      return {time:`${formattedTime}` , date:`${day} ${month} ${year}`};
+    }
+
+    //updates the active group Id with the respective notes container
+    const handleNoteSubmit = (noteText) => {
+      if (activeGroupId) {
+        const updatedGroups = groups.map((group) => {
+          if (group.id === activeGroupId) {
+            const newNote = {
+              id: Date.now(),
+              text: noteText,
+              timestamp: getTimeStamp()
+            };
+            return {
+              ...group,
+              notes: [...group.notes, newNote]
+            };
+          }
+          return group;
+        });
+        setGroups(updatedGroups);
+      }
+    };
+
+    //finds the active group and Sets the active group with the respective group id
+    const activeGroup = groups.find((group) => group.id === activeGroupId);
+
+    //Group Initials are the group name start letters
+    const getGroupInitials = (groupName) => {
+      const initials = groupName
+        .trim()
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase())
+        .join('');
+      return initials.slice(0, 2);
+    };
+      
+      
+    return(
+      
+      // The main groups Container which handles groups name and  color and the initials includes mobile view with state showbutton
+    <div className="main">    
+      {showButton && (
+        <div className="groups-container">
+          <h1 className="name">Pocket Notes</h1>
+          <button className="creating-group-btn" onClick={()=>setIsPopUpVisible(true)}><span className="plus">+ Create Notes group</span></button>
+          <div className="groupNamesContainer">
+            {groups.map((group)=> (
+                < div key={group.id} className={`grp-card ${activeGroupId === group.id ? 'active' : ''}`} onClick={() => handleGroupClick(group.id)}>
+                  <button className="grp-btn" style={{ backgroundColor: group.color }}>
+                  {group.name.trim().split(' ').map((word, index) => (
+                      <span key={index} className="groupIconName">
+                      {index === 0 && word.charAt(0).toUpperCase()}
+                      {index === 1 && word.charAt(0).toUpperCase()}
+                      </span>
+                  ))}
+                  </button>
+                  <p className="grp-Name">{group.name}</p>
+                </div>
+            ) )}
+          </div>
+        </div>)}
+
+          {/* When there is no active group or groups are not created yet then this container will be displayed */}
+        {(groups.length === 0 || !activeGroup)&&
+        <div className="notes-container">
+            <img src={bgimg} alt="img" className="image"></img>
+            <h1 className="notes-heading">Pocket Notes</h1>
+            <p className="para">Send and receive messages without keeping your phone online.                   Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p> 
+            <p><img src={lock} alt="lock" className="lock-img"></img><span className="footer">end-to-end encrypted</span></p>    
+        </div>}
+
+          {/* Passing props to notes component and if activeGroup is there then only the respective notes will be opened   */}
+          {activeGroupId && (
+              <Notes
+              groupInitials={getGroupInitials(groups.find((group) => group.id === activeGroupId).name)}
+              activeGroup={activeGroup}
+              onNoteSubmit={handleNoteSubmit}
+              isVisible={isVisible}
+              setIsVisible={setIsVisible}
+              setShowButton={setShowButton}
+              />
+          )}
+      </div>
+    )
+}
+
+export default Groups
---
+++ src/Components/Notes.js
@@ -0,0 +1,71 @@
+
+//Notes Component which contains enter notes and added notes along with its timestamp
+import React , {useState} from "react";
+import './Notes.css';
+
+function Notes({ onNoteSubmit,activeGroup,groupInitials,isVisible,setIsVisible,setShowButton}){
+  //Destructuring props passed by the groups component
+
+  //Note text state management for entering the notes
+  const [noteText,setNoteText]=useState([]) 
+
+  //Handles the enter button click event when user tries to add a note text in the notes container
+  const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        handleSubmit();
+      }
+  };
+
+  //Handles the submit event When the note entered by the user
+  const handleSubmit = () => {
+      if (noteText.trim() !== '') {
+        onNoteSubmit(noteText);
+        setNoteText('');
+      }
+    };
+
+  //Handles Arrow click which is only available on mobile view manages the states is visible and show button
+  const handleArrowClick = () => {
+      setIsVisible(false);
+      setShowButton(true);
+  }
+    
+  return(
+    <div className="main-notes">
+      {isVisible && (
+      <div   className="add-note-container"        
+      style={{
+      display:"block", // Display as block for wider screens (web view)
+      '@media (max-width: 480px)': {
+        display: isVisible ? 'block' : 'none', //for mobile view
+      }
+      }}>
+        <div className="notes-grp-card" >
+            <p className="arrow" onClick={handleArrowClick}>&#8592;</p> {/*arrow for mobile view */}
+            <button className="notes-grp-btn" style={{backgroundColor:activeGroup.color}} ><span className="groupIconName">{groupInitials}</span></button>
+            <p className="notes-grp-Name">{activeGroup.name}</p>
+        </div>
+        <div className="note-card">
+          {/* notes added card */}
+          <ul>
+          {activeGroup.notes.map((note)=> (
+              <li className="addedNotes" key={note.id}>
+                <div className="noteId">
+                  <div className="notesTime">{note.timestamp.time}</div>
+                  <div className="notesDate">{note.timestamp.date}</div>
+                </div>
+                <div className="notesText">{note.text}</div>
+              </li>))}
+          </ul>
+        </div> 
+          {/* entering notes area */}
+        <form onSubmit={handleSubmit} className="enter-notes">
+            <textarea rows="20" col="500" placeholder="Enter your text here......" className="input-card" value={noteText} onKeyPress={handleKeyPress} onChange={(e)=>setNoteText(e.target.value)} />
+            <button type="submit" className="enter-btn" ></button>
+        </form>
+      </div>)}
+    </div>
+  )
+}
+
+export default Notes
